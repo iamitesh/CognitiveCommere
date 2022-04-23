c57bc19ae7c80469a38a01816f8e34f11f990031
Amitesh Anand <aamitesh7@gmail.com>
2022-04-24 00:04:47 +0530

    Code cleanup and deploy link correction

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const  App = () =>{
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        This is header content
       </header>
+      <main className="main">
+      This is main content
+      </main>
+      <footer className="footer">
+        This is footer content
+      </footer>
     </div>
   );
 }
