f12fd58b3c8db74e62b035e83c9cb003294794b1
Amitesh Anand <aamitesh7@gmail.com>
2022-04-24 20:22:44 +0530

    logo and brandname update

--- src/Components/common/Navbar.js
+++ src/Components/common/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
           <nav className="navbar navbar-expand-lg navbar-light bg-blue">
             <div className="container-fluid">
               <Link className="navbar-brand" to="#">
-                Navbar
+                Cognitive Commerce
               </Link>
               <button
                 className="navbar-toggler"
